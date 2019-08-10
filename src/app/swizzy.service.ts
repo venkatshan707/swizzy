36592cbbe465eac37569a1bf962db9106f6f1684
venkatshan707 <venkatshan707@gmail.com>
2019-08-10 17:30:53 +0530

    passing value to next page

--- src/app/swizzy.service.ts
+++ src/app/swizzy.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe';
 import {RECIPE} from './recipe-sample-data'
 
+
 @Injectable()
 export class SwizzyService {
 
-  constructor() { }
 
+  constructor() { }
+  getRecipe(neededName: any): Recipe {
+    for(var i=0;i<RECIPE.length;i++){
+      if(RECIPE[i].name==neededName)
+      return RECIPE[i];}
+  }
   recipeNameEntered(name: any): Recipe{
     for(var i=0;i<RECIPE.length;i++){
       if(RECIPE[i].name==name)
