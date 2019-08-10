51d4c2161ff6b85d6ec9904f5d396cfdc419643c
venkatshan707 <venkatshan707@gmail.com>
2019-08-10 22:26:01 +0530

    added some recipe data

--- src/app/recipe-sample-data.ts
+++ src/app/recipe-sample-data.ts
@@ -2,5 +2,8 @@ import {Recipe} from "./recipe";
 export const RECIPE:Recipe[]=[
   {name:'idly',  price :40  },
   {name:'dhosa',  price :40  },
+  {name:'biriyani',  price :140  },
+  {name:'pongal',  price :40  },
+  {name:'fish',  price :150 },
 
 ]
